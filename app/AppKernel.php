536e7fff27ecfe49b8cfe8b4716c4999bcdc710c
Omar Rodríguez <oarozu@gmail.com>
2020-08-02 16:56:17 -0300

    fix-appk

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Admin\UserBundle\AdminUserBundle(),
             new Admin\UnadBundle\AdminUnadBundle(),
+            new Admin\UserBundle\AdminUserBundle(),
             new Admin\MedBundle\AdminMedBundle(),
         );
 
